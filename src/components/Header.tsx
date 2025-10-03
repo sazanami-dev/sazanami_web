8b6b1f0a0c373325d13b5e3140b46bddac5dfb99
Asura146 <kyo008g@gmail.com>
2025-10-03 09:28:05 +0900

    Headerコンポーネントにさざなみ開発のロゴを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,16 @@
+import Image from 'next/image';
+
 export const Header = () =>{
     return (
         <>
+        <Image
+          className=""
+          src="/sazanami_dev.svg"
+          alt="sazanami_dev.svg"
+          width={180}
+          height={38}
+          priority
+        />
         </>
     );
 }
