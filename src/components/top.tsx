25fb6e6905754e51648d4d2a0b314ea01fabaee4
Asura146 <kyo008g@gmail.com>
2025-10-08 12:39:27 +0900

    背景画像を表示

---
+++ src/components/top.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const Top = () => {
+  return (
+    <>
+      <Image
+        src="background_img.svg"
+        alt="background_img"
+        className="ml-auto"
+        width={1600}
+        height={38}
+        priority
+        objectFit="contain"
+      />
+    </>
+  );
+};
