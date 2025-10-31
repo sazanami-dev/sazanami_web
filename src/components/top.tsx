6d561747d6b806d654d71928b12918373242a218
Asura146 <kyo008g@gmail.com>
2025-11-01 01:43:19 +0900

    背景画像上にメイン画像を追加

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -7,11 +7,19 @@ export const Top = () => {
       <Image
         src="background_img.svg"
         alt="background_img"
-        className="ml-auto opacity-40"
+        className="ml-auto opacity-40 object-contain"
         width={1600}
         height={38}
         priority
         objectFit="contain"
+      />
+      <Image
+        src="/main.JPG"
+        alt="main"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        width={400}
+        height={300}
+        priority
       />
     </>
   );
