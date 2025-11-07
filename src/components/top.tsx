2f628174a1282bc9e5d5d47971d3914ddebcd640
Asura146 <kyo008g@gmail.com>
2025-11-07 18:46:33 +0900

    モバイルで表示した際にロゴが下に来るようにした

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -13,22 +13,22 @@ export const Top = () => {
         priority
         objectFit="contain"
       />
-      <div className="absolute top-0 left-0 w-full h-screen flex items-center">
-        <div className="w-1/2 flex justify-center">
+      <div className="absolute top-0 left-0 w-full h-screen flex flex-col md:flex-row items-center md:items-center justify-center md:justify-normal">
+        <div className="w-full md:w-1/2 flex justify-center order-2 md:order-1 mt-6 md:mt-0">
           <Image
             src="/log-phrase.svg"
             alt="sazanami_dev_logo"
-            className="object-contain w-[40vw] sm:w-[40vw]"
+            className="object-contain w-[55vw] sm:w-[45vw] md:w-[40vw]"
             width={800}
             height={300}
             priority
           />
         </div>
-        <div className="w-1/2 flex justify-center">
+        <div className="w-full md:w-1/2 flex justify-center order-1 md:order-2">
           <Image
             src="/main.svg"
             alt="main"
-            className="object-contain w-[35vw] sm:w-[35vw]"
+            className="object-contain w-[60vw] sm:w-[50vw] md:w-[35vw]"
             width={650}
             height={300}
             priority
