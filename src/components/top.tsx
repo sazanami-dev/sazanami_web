ccc43817e7e6de7005f6a4ae88d062e3e24802ad
Asura146 <kyo008g@gmail.com>
2025-11-10 01:18:02 +0900

    TopコンポーネントのImageタグのインデントを修正し、背景画像の高さを100svhに変更

--- src/components/top.tsx
+++ src/components/top.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Top = () => {
   return (
     <div className="relative w-full min-h-screen">
-    <Image
+      <Image
         src="background_img.svg"
         alt="background_img"
         className="ml-auto opacity-40 object-contain"
@@ -13,7 +13,7 @@ export const Top = () => {
         priority
         objectFit="contain"
       />
-      <div className="absolute top-0 left-0 w-full h-screen flex flex-col md:flex-row items-center md:justify-normal pl-6 pr-6 pt-15 md:pl-0 md:pr-0">
+      <div className="absolute top-0 left-0 w-full h-[100svh] md:h-screen flex flex-col md:flex-row items-center md:items-center md:justify-normal pl-6 pr-6 pt-15 md:pt-0 md:pl-0 md:pr-0">
         <div className="w-full md:w-1/2 flex md:justify-center order-2 md:order-1 mt-15 md:mt-0">
           <Image
             src="/log-phrase.svg"
