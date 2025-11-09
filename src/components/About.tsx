277d35193a9738d400f59ffb4baa9617107bd29d
Asura146 <kyo008g@gmail.com>
2025-11-09 20:14:51 +0900

    文章を追加

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,15 +2,19 @@ import Image from "next/image"
 export const About = () => {
 	return(
 		<>
-			<div className="-mt-72 sm:ml-20">
+			<div className="-mt-72 sm:ml-32">
 				<Image
 					src="/about-sazanami.svg"
 					alt="about-sazanami_dev"
-					className="object-contain w-[70vw] sm:w-[35vw] md:w-[35vw]"
+					className="object-contain w-[70vw] sm:w-[40vw] md:w-[35vw]"
 					width={800}
 					height={300}
 					priority
 				/>   
+			</div>
+			<div className="ml-10 mr-10">
+				<p>さざなみ開発はトライデントコンピュータ専門学校公認の技術系サークルです。
+				このサークルでは勉強会やハッカソン、LTなどを開催しています。開発をしたい人や知識を持った人たちに出会える場を提供しています。</p>
 			</div>
 		</>
 	);
