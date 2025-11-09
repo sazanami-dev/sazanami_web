bba209d17d235e6837caee1d06dbc6bf1d6476ca
Asura146 <kyo008g@gmail.com>
2025-11-10 00:24:17 +0900

    Aboutコンポーネントのレイアウトとスタイルを改善

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
 export const About = () => {
 	return(
-		<>
-			<div className="-mt-72 lg:ml-32">
+		<div className="-mt-72 mb-30">
+			<div className="ml-3 sm:ml-15 lg:ml-32 xl:ml-50 2xl:ml-80">
 				<Image
 					src="/about-sazanami.svg"
 					alt="about-sazanami_dev"
@@ -12,10 +12,12 @@ export const About = () => {
 					priority
 				/>   
 			</div>
-			<div className="mt-3 ml-10 mr-10 sm:ml-20 sm:mr-20 md:ml-30 md:mr-30 lg:ml-60 lg:mr-60 2xl:ml-100 2xl:mr-100">
-				<p className="font-sans font-extrabold sm:text-2xl">さざなみ開発はトライデントコンピュータ専門学校公認の技術系サークルです。
-				このサークルでは勉強会やハッカソン、LTなどを開催しています。開発をしたい人や知識を持った人たちに出会える場を提供しています。</p>
+			<div className="mt-3 ml-10 mr-10 sm:ml-20 sm:mr-20 md:ml-30 md:mr-30 lg:ml-60 lg:mr-60 xl:ml-70 xl:mr-80 2xl:ml-120 2xl:mr-120">
+				<p className="font-sans font-semibold text-base sm:text-xl md:text-xl xl:text-2xl">
+					さざなみ開発はトライデントコンピュータ専門学校公認の技術系サークルです。
+					このサークルでは勉強会やハッカソン、LTなどを開催しています。開発をしたい人や知識を持った人たちに出会える場を提供しています。
+				</p>
 			</div>
-		</>
+		</div>
 	);
 }
